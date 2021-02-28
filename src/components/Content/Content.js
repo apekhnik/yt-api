c8aeb9ca4868ce5db1043e12e762425129c3ce95
apekhnik <iam3ji@gmail.com>
2021-02-28 09:59:54 +0300

    fix

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,17 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import YouTube from "react-youtube";
 import cl from "classnames";
-import { getSelectedVideo, getVideoList } from "../../redux/appSlice";
+import {
+  getSelectedVideo,
+  getVideoList,
+  setVideoList,
+} from "../../redux/appSlice";
 import PreviewVideoItem from "../previewItem/PreviewVideoItem";
 import "./style.sass";
 const Content = () => {
   const selectedVideo = useSelector(getSelectedVideo);
+  const dispatch = useDispatch();
   const videoList = useSelector(getVideoList);
   const opts = {
     height: "360",
@@ -23,12 +28,21 @@ const Content = () => {
         key={index}
         url={item.snippet.thumbnails.medium.url}
         title={item.snippet.title}
+        channelTitle={item.snippet.channelTitle}
+        publishedAt={item.snippet.publishedAt}
       />
     );
   });
   const classes = cl(
     !selectedVideo ? "searchList__full" : "searchList__sidebar"
   );
+  useEffect(async () => {
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${"cat"}&maxResults=50&type=video&key=AIzaSyAm8O8_eJFmFCJkSjuWaLCCOjwDBxd4lmw`
+    ).then((response) => response.json());
+
+    dispatch(setVideoList(response.items));
+  }, []);
   return (
     <div className="content">
       <div className="sidebar"></div>
--- src/components/previewItem/PreviewVideoItem.js
+++ src/components/previewItem/PreviewVideoItem.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import ReactTimeago from "react-timeago";
 import { setSelectedVideo } from "../../redux/appSlice";
 import "./style.sass";
-const PreviewVideoItem = ({ url, title, videoId }) => {
+const PreviewVideoItem = ({
+  url,
+  title,
+  videoId,
+  channelTitle,
+  publishedAt,
+}) => {
   const dispatch = useDispatch();
-
+  const correctedTitle = (title) => `${title.slice(0, 35)}...`;
+  const videoTitle = correctedTitle(title);
   return (
     <div
-      className="article"
+      className="videoItem"
       onClick={() => dispatch(setSelectedVideo(videoId))}
     >
       <img src={url} />
-      <h3>{title}</h3>
+      <h3>{videoTitle}</h3>
+      <h4>{channelTitle}</h4>
+      <span>
+        <ReactTimeago date={publishedAt} />
+      </span>
     </div>
   );
 };
